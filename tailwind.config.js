06ce4585b422d5dda8c40eb1678e214f4ade173c
reiearth <reionlinebiz@gmail.com>
2022-01-22 22:07:47 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#17171F',
+        'selected-text':'#a3a3ff',
+        'theme':'#3f3fff',
+        'nav':'#404053',
+        'secondary':'#9191A4',
+        'badge': '#3f3f51',
+        'input-border':'#565666',
+        'input':'#2a2a35'
+      },
+      fontFamily: {
+        'poppins': ["'Poppins'", 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
